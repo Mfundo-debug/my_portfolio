58757e2875b4ff6a0bcb6e5f1e91515658b15250
Mfundo_debug <diditmfundo@gmail.com>
2023-06-30 13:39:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*================ toggle icon navbar ================*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+/*================ scroll sections active link ================*/
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", scrollActive);
+window.onscroll = function () {
+/*======== sticky navbar ===========*/
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+/*======== remove toggle icon and navbar when clicking link (scroll) ===========*/
+    navToggler.classList.remove("active");
+    navMenu.classList.remove("open");
+};
+/*======== scroll reveal  ===========*/
+window.sr = ScrollReveal();
+sr.reveal(".animate-left", {
+    origin: "left",
+    duration: 1000,
+    distance: "25rem",
+    delay: 300,
+});
+/*======== typed js  ===========*/
+var typed = new Typed(".typing", {
+    strings: ["Data Scientist", "Researcher", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
