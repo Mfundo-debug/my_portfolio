09b5e157fc585f1080741f3ab606d51e364fe544
Mfundo_debug <diditmfundo@gmail.com>
2023-07-01 16:34:49 +0200

    undeplooyed website with no links attached

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*================ toggle icon navbar ================*/
-const menuIcon = document.querySelector(".menu-icon");
+const menuIcon = document.querySelector("#menu-icon");
 const navbar = document.querySelector(".navbar");
 
 menuIcon.addEventListener("click", () => {
@@ -9,28 +9,51 @@ menuIcon.addEventListener("click", () => {
 
 
 /*================ scroll sections active link ================*/
-const sections = document.querySelectorAll("section[id]");
-window.addEventListener("scroll", scrollActive);
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+
+
 window.onscroll = function () {
-/*======== sticky navbar ===========*/
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-/*======== remove toggle icon and navbar when clicking link (scroll) ===========*/
-    navToggler.classList.remove("active");
-    navMenu.classList.remove("open");
+    sections.forEach(sec => {
+        top = window.scrollY;
+        offset = sec.offsetTop - 150;
+        height = sec.offsetHeight;
+        id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active" );
+            });
+        };
+    });
 };
+
+/*======== sticky navbar ===========*/
+header = document.querySelector('header')
+header.classList.toggle('sticky', window.scrollY>100);
+   
+/*======== remove toggle icon and navbar when clicking link (scroll) ===========*/
+ menuIcon.classList.remove("bx-x");
+ navbar.classList.remove("active");   
 /*======== scroll reveal  ===========*/
-window.sr = ScrollReveal();
-sr.reveal(".animate-left", {
-    origin: "left",
-    duration: 1000,
-    distance: "25rem",
-    delay: 300,
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
 });
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-img, .service-container, .protfolio-box, .contact-form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+
+
 /*======== typed js  ===========*/
-var typed = new Typed(".typing", {
+const typed = new Typed(".multiple-text", {
     strings: ["Data Scientist", "Researcher", "Freelancer"],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 100,
+    backDelay: 1000,
     loop: true,
 });
